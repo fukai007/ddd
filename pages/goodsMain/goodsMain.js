c77f3566ad3e6c7911c87b3b657939484c432c92
fukai <fukai@kuchuan.com>
2018-02-05 22:44:50 +0800

    修改样式

--- pages/goodsMain/goodsMain.js
+++ pages/goodsMain/goodsMain.js
@@ -10,7 +10,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
@@ -30,42 +30,42 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
@@ -86,6 +86,6 @@ Page({
   toga:function(){
     app.toPage('askForGoods', {
       g_id: this.data.gd.g_id
-    });
+    },'to');
   }
 })
